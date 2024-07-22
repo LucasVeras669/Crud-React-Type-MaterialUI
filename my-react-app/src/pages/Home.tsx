e306d03a6b022c296fb8d25a2f1968fd4762ab3b
Lucas Veras Ferreira <lucasveras@sefaz.pi.gov.br>
2024-07-22 08:07:38 -0300

    Adicionado biblioteca material-ui

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material"
 
 
 export const Home = () => {
     return (
       <>
         <h1>Olá Mundo</h1>
+        <Button>Ola Mundo</Button>
       </>
     )
   }
